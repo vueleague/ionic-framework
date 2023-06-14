8f9e6dc2bf86cf70cda4c2d0b9a11bebd41b8518
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-06-14 10:38:18 -0400

    remove package sync scripts

--- packages/react/scripts/sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Copy core dist
-rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
-cp -a ../../core/dist node_modules/@ionic/core/dist
-cp -a ../../core/components node_modules/@ionic/core/components
-cp -a ../../core/package.json node_modules/@ionic/core/package.json
